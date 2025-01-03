09aa6cd52532e1a5d5783cbdbbea96be3a0cb348
Akkenapally Ruthwik Reddy <131450727+ruthwwikreddy@users.noreply.github.com>
2025-01-03 13:02:10 +0530

    Delete assets/browser-dkxags2u-DbsR88Yj.js

--- assets/browser-dkxags2u-DbsR88Yj.js
+++
@@ -1,1 +0,0 @@
-import{y as e}from"./index-BDjsgaze.js";var o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")};const t=e(o),n=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"}));export{n as b};
